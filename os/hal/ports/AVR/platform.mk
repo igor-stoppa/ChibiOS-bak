3436688aa6d6b26533c55b952168ce2e8918ae31
Igor Stoppa <igor.stoppa@gmail.com>
2015-09-03 22:58:07 +0300

    HAL: AVR: External Interrupts Driver
    The ATMega328p found on Arudino boards supports 2 ways of generating
    interrupts from external events:
    
    * direct interrupts - each of the two lines (INT0 and INT1) can be
      programmed to react to specific conditions.
    
    * pin change interrupts - all the other GPIOS do not have a dedicated
      interrupt line. Instead, they are grouped into PORTs and each port has
      an interrupt line that fires when any of the GPIOs belonging to said
      port changes state. It is also possible to mask GPIOs that can be ignored.
    
    This low level driver enables the use of both modes.
    
    Signed-off-by: Igor Stoppa <igor.stoppa@gmail.com>

--- os/hal/ports/AVR/platform.mk
+++ os/hal/ports/AVR/platform.mk
@@ -1,6 +1,7 @@
 # List of all the AVR platform files.
 PLATFORMSRC = ${CHIBIOS}/os/hal/ports/AVR/hal_lld.c \
               ${CHIBIOS}/os/hal/ports/AVR/hal_pal_lld.c \
+              ${CHIBIOS}/os/hal/ports/AVR/hal_ext_lld.c \
               ${CHIBIOS}/os/hal/ports/AVR/hal_serial_lld.c \
               ${CHIBIOS}/os/hal/ports/AVR/hal_adc_lld.c \
               ${CHIBIOS}/os/hal/ports/AVR/hal_i2c_lld.c \
